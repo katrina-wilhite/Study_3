7f2d4056d97f76fe0b08427df2984d925f2440af
Katrina Wilhite <katrina.wilhite@gmail.com>
2022-08-26 08:37:49 +1000

    Updated dataframe and tried decomp function

--- simple_mediation.R
+++ simple_mediation.R
@@ -22,7 +22,7 @@ df_male_domsp_weekday %>%
 lsac_wave6 %>% 
   select(hicid, fcnfsad2d, fcnfsda2d, fcnfser2d, fcnfseo2d, fmoth, ffath, fsc13a1i, zf09fm, zf09ff) -> wave_6_relevant_data
 #Update column names to make sense 
-colnames(wave_6_relevant_data)[2:10] <- c("advantage_disadvantage", "disadvantage", "resources", "education_occupation", "mother_home", "father_home", "mental_health", "mother_ethnicity", "father_ethnicity")
+colnames(wave_6_relevant_data)[2:10] <- c("advantage_disadvantage", "disadvantage", "resources", "education_occupation", "mother_home", "father_home", "mental_health", "mother_race", "father_race")
 
 #Select relevant columns from LSAC Wave 8 (SDQ results) 
 lsac_wave8 %>% 
@@ -39,7 +39,24 @@ colSums(is.na(df_males))
 #11 participants are missing SDQ data - this accounts for <1% of data and this information is essential for data anlaysis - therefore these participants will be removed from the dataset 
 df_males_na_removed <- na.omit(df_males)
 
+
+df_males_na_removed$model3_trajectory_assignments <- as.factor(df_males_na_removed$model3_trajectory_assignments) 
 #Run analysis 
-expData <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_ethnicity + father_ethnicity, data = df_males_na_removed)
-neProsocial <- neModel(prosocial ~ SEP0 + SEP1 + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_ethnicity + father_ethnicity, expData = expData, se = "robust")
+df_males_na_removed[,"prosocial"] %>% 
+  mutate_at(("prosocial"), ~(scale(.) %>% as.vector)) -> df_males_scaled
+
+
+expFit <- glm(model3_trajectory_assignments*SEP ~ Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
+expData <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
+neProsocial <- neModel(prosocial ~ SEP0 + SEP1 + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, expData = expData, se = "robust")
 summary(neProsocial)
+str(neProsocial)
+unlist(neProsocial) -> neProsocial2
+
+effdecomp <- neEffdecomp(neProsocial)
+#Try making a fitted model when I get home. 
+summary(effdecomp)
+
+summary(expData)
+head(expData)
+expData2 <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_ethnicity + father_ethnicity, family = poisson(link = "log"), data = df_males_na_removed)
