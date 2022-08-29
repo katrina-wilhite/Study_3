8dc0fa9cb4ffbc62a1393cc3dd5d6b3e0d77bd49
Katrina Wilhite <katrina.wilhite@gmail.com>
2022-08-29 17:34:07 +1000

    Update interaction terms and run male mediation

--- mediation.R
+++ mediation.R
@@ -21,6 +21,7 @@ relevant_wave8_data <- lsac_wave8 %>%
 inner_join(df_male_domsp_weekday, relevant_wave6_data, by = "hicid") -> domsp_wave6
 
 inner_join(domsp_wave6, relevant_wave8_data, by = "hicid") -> df_males
+df_males <- na.omit(df_males)
 
 #Select mediator variables (domain-specific movement behaviours)
 df_males %>% 
@@ -30,68 +31,33 @@ df_males %>%
 mediator_list=as.list(mediator_variables)
 
 matrix(unlist(mediator_list), ncol = 13, byrow = FALSE) -> mediator_matrix
-colnames(mediator_matrix) <- c("1_active_transport", "2_naps", "3_education", "4_leisure_SB", "5_passive_transport", "6_screen_time", "7_self_care", "8_social", "9_sports", "10_unst_LPA", "11_unst_MVPA", "12_household", "13_sleep")
+colnames(mediator_matrix) <- c("v1_active_transport", "v2_naps", "v3_education", "v4_leisure_SB", "v5_passive_transport", "v6_screen_time", "v7_self_care", "v8_social", "v9_sports", "v10_unst_LPA", "v11_unst_MVPA", "v12_household", "v13_sleep")
 df_mediators <- as.data.frame(mediator_matrix)
-#Scale and center mediators
-scale(df_mediators, scale = FALSE) -> df_mediators
+#Scale and center mediators - scale function centers by default 
+scale(df_mediators) -> df_mediators
 #Center mediators
 str(df_mediators)
 
-interactions_2 <- t(apply(df_mediators, 1, combn, 2, prod))
-colnames(interactions_2) <- paste(combn(1:13, 2, paste, collapse="V"), sep = "_")
-interactions_3 <- t(apply(df_mediators, 1, combn, 3, prod))
-colnames(interactions_3) <- paste(combn(1:13, 3, paste, collapse="V"), sep = "_")
-interactions_4 <- t(apply(df_mediators, 1, combn, 4, prod))
-colnames(interactions_4) <- paste(combn(1:13, 4, paste, collapse="V"), sep = "_")
-interactions_5 <- t(apply(df_mediators, 1, combn, 5, prod))
-colnames(interactions_5) <- paste(combn(1:13, 5, paste, collapse="V"), sep = "_")
-interactions_6 <- t(apply(df_mediators, 1, combn, 6, prod))
-colnames(interactions_6) <- paste(combn(1:13, 6, paste, collapse="V"), sep = "_")
 
-interactions_7 <- t(apply(df_mediators, 1, combn, 7, prod))
-colnames(interactions_7) <- paste(combn(1:13, 7, paste, collapse="V"), sep = "_")
-
-interactions_8 <- t(apply(df_mediators, 1, combn, 8, prod))
-colnames(interactions_8) <- paste(combn(1:13, 8, paste, collapse="V"), sep = "_")
-
-interactions_9 <- t(apply(df_mediators, 1, combn, 9, prod))
-colnames(interactions_9) <- paste(combn(1:13, 9, paste, collapse="V"), sep = "_")
-
-interactions_10 <- t(apply(df_mediators, 1, combn, 10, prod))
-colnames(interactions_10) <- paste(combn(1:13, 10, paste, collapse="V"), sep = "_")
-
-interactions_11 <- t(apply(df_mediators, 1, combn, 11, prod))
-colnames(interactions_11) <- paste(combn(1:13, 11, paste, collapse="V"), sep = "_")
-
-interactions_12 <- t(apply(df_mediators, 1, combn, 12, prod))
-colnames(interactions_12) <- paste(combn(1:13, 12, paste, collapse="V"), sep = "_")
-
-cbind(interactions_2, interactions_3, interactions_4, interactions_5, interactions_6, interactions_7, interactions_8, interactions_9, interactions_10, interactions_11, interactions_12) -> df_mediators
-
-mediator_interactions <- as.matrix(df_mediators)
-str(mediator_interactions)
-
-
-interactions_13 <- t(apply(df_mediators, 1, combn, 13, prod))
-colnames(interactions_13) <- paste(combn(1:13, 13, paste, collapse="V"), sep = "_")
-
-
-for (col in 1:ncol(df_mediators)) {
-  t(apply(1, combn, 2:13, prod))
+for (x in 2:12){
+  assign(paste0("interactions_", x), (t(apply(df_mediators, 1, combn, x, prod))))
+  interaction_number <- get(paste0("interactions_",x))
+  colnames(interaction_number) <- paste(combn(1:13, x, paste, collapse="V"), sep = "_")
+  assign(paste0("interactions_", x), interaction_number)
 }
 
+interactions_13 <- df_mediators[,1]*df_mediators[,2]*df_mediators[,3]*df_mediators[,4]*df_mediators[,5]*df_mediators[,6]*df_mediators[,7]*df_mediators[,8]*df_mediators[,9]*df_mediators[,10]*df_mediators[,11]*df_mediators[,12]*df_mediators[,13]
+interactions_13 <- as.matrix(interactions_13)
+colnames(interactions_13) <- "1V2V3V4V5V6V7V8V9V10V11V12V13"
 
-mediator_interactions_2 <- lapply(1:(ncol(combn(1:ncol(df_mediators), m = 2))),
-                                function(y) df_mediators[, combn(1:ncol(df_mediators), m = 2)[,y]])
-str(mediator_interactions_2)
-
+cbind(interactions_2, interactions_3, interactions_4, interactions_5, interactions_6, interactions_7, interactions_8, interactions_9, interactions_10, interactions_11, interactions_12, interactions_13) -> mediators_interactions
 #Ensure structure of "mediator_matrix" is the same as "med' in the regmed vignette 
-##Run first part of regmed vignette to get data 
-data(medsim)
-dat.filter <- regmed.prefilter(x[, 1], med, y[, 1], k = 10)
-str(dat.filter$med)
+##Run first part of regmed vignette to see data 
+#data(medsim)
+#dat.filter <- regmed.prefilter(x[, 1], med, y[, 1], k = 10)
+#str(dat.filter$med)
 ##Check structure of "mediator_matrix"
-str(mediator_matrix)
+#str(mediators_interactions)
 #Structure matches so the mediation variable should be set 
 
 #Select exposure variables (SEP)
@@ -99,88 +65,108 @@ df_males %>%
   select(SEP) -> exposure_variable
 str(dat.filter$x)
 str(exposure_variable)
-exposure_variable$SEP -> exposure_SEP_only
-str(exposure_SEP_only)
-as.matrix(exposure_SEP_only)
-exposure_SEP_only <- scale(exposure_SEP_only, scale = FALSE)
-str(exposure_SEP_only)
-#Does not match dat.filter$x structure
-as.matrix(exposure_variable) -> exposure_matrix 
-str(exposure_matrix)
-#Does not match structure
-as.array(exposure_matrix) -> exposure_array
-str(exposure_array)
-#Does not match structure
-exposure_list=as.list(exposure_variable)
-str(exposure_list)
-#Does not match structure
-t(exposure_matrix) -> transposed_exposure_matrix
-str(transposed_exposure_matrix)
-unname(transposed_exposure_matrix) -> transposed_exposure_matrix2
-str(transposed_exposure_matrix2)
-dim(transposed_exposure_matrix2) <- c(1,1025)
-str(transposed_exposure_matrix2)
-attributes(transposed_exposure_matrix2)
-dim(transposed_exposure_matrix2)
-transposed_exposure_matrix2[c(1,1)]
-transposed_exposure_matrix2[,2]
-transposed_exposure_matrix2[c(1,2)]
+exposure_variable$SEP -> exposure_variable
+str(exposure_variable)
 
 
-help(regmed)
+df_males[, 124:129]
+for (x in 124:ncol(df_males)) {
+  colnames(df_males[,x]) -> name
+  assign(paste(name), df_males %>% 
+    select(x) %>%
+    scale())
+ assign(paste0(name),get(name)[,1])
+}
 
-#Select outcome variables (SDQ results)
-df_males %>% 
-  select(fcnfsad2) -> outcome_variable
-data.matrix(outcome_variable)
-outcome_variable$fcnfsad2 -> outcome_SDQ_only
-as.matrix(outcome_SDQ_only)
-outcome_SDQ_only <- scale(outcome_SDQ_only, scale = FALSE)
-str(outcome_SDQ_only)
-as.matrix(outcome_variable) -> outcome_matrix
-str(outcome_variable)
-str(outcome_matrix)
-head(outcome_matrix)
-t(outcome_matrix) -> transposed_outcome_matrix
-str(transposed_outcome_matrix)
-unname(transposed_outcome_matrix) -> transposed_outcome_matrix2
-str(transposed_outcome_matrix2)
-#Hizzah! 
+#str(hapsoc)
+#str(hapeer)
+#Wooo! 
 
-#Combine new exposure, mediator, and outcome variables into a list 
-male_lasso_data <- list(transposed_exposure_matrix2, mediator_matrix, transposed_outcome_matrix2)
-str(male_lasso_data)
 
-male_lasso_2 <- list(exposure_SEP_only, mediator_matrix, outcome_SDQ_only)
-str(male_lasso_2)
-names(male_lasso_2) <- c("exposure", "mediator", "outcome")
-str(male_lasso_2)
-x <- male_lasso_2$exposure
-y <- male_lasso_2$outcome
-med <- male_lasso_2$mediator
-#Check to ensure the strucutre is the same as the regmed vignette 
-str(dat.filter)
-str(male_lasso_data)
-#Yay! 
-names(male_lasso_data) <- c("exposure", "mediator", "outcome")
-str(male_lasso_data$mediator)
-x <- male_lasso_data$exposure
-y <- male_lasso_data$outcome
-med <- male_lasso_data$mediator
+#male_lasso_prosocial <- list(exposure_variable, mediators_interactions, hapsoc)
+#names(male_lasso_prosocial) <- c("exposure", "mediator", "outcome")
+#x <- male_lasso_prosocial$exposure
+#y <- male_lasso_prosocial$outcome
+#med <- male_lasso_prosocial$mediator
+#str(male_lasso_prosocial)
+#Check to ensure the structure is the same as the regmed vignette 
+
+cbind(hapsoc, hahypr, haemot, hapeer,  hacondb, hasdqtb) -> df_outcomes
+class(df_outcomes)
+df_outcomes <- as.data.frame(df_outcomes)
+class(df_outcomes)
 
 lambda.grid <- seq(from = 0.4, to = 0.01, by = -0.01)
 
-fit.male <- regmed.grid(x, med, y, lambda.grid, frac.lasso = 0.8)
+for(i in 1:ncol(df_outcomes)) {
+  colnames(df_outcomes)[i] -> outcome
+  assign(paste0("male_lasso_",outcome), list(exposure_variable, mediators_interactions, df_outcomes[,i])) -> outcome_list
+  names(outcome_list) <- c("exposure", "mediator", "outcome")
+  x <- outcome_list$exposure
+  y <- outcome_list$outcome
+  med <- outcome_list$mediator
+  assign(paste0("dat.filter.", outcome), regmed.prefilter(x, med, y)) -> dat.filter
+  x1 <- dat.filter$x
+  y1 <- dat.filter$y
+  med <- dat.filter$mediator
+}
 
-plot.regmed.grid(fit.male)
 
-fit.best <- regmed.grid.bestfit(fit.male)
-summary(fit.best)
 
-edges.med <- regmed.edges(fit.best, type = "mediators")
+assign(paste0("fit.male.", outcome), regmed.grid(x1, med, y1, lambda.grid, frac.lasso = 0.8)) 
+
+str(male_lasso_hapsoc$outcome)
+
+data(male_lasso_prosocial)
+dat.filter.prosocial <- regmed.prefilter(x, med, y)
+
+
+
+x1 <- dat.filter.prosocial$x
+y1 <- dat.filter.prosocial$y
+med <- dat.filter.prosocial$mediator
+
+fit.male.prosocial <- regmed.grid(x1, med, y1, lambda.grid, frac.lasso = 0.8)
+
+plot.regmed.grid(fit.male.prosocial)
+
+fit.best.prosocial <- regmed.grid.bestfit(fit.male.prosocial)
+summary(fit.best.prosocial)
+
+fit.single <- regmed.fit(x1, med, y1, lambda = .12, frac.lasso =0.8)
+summary(fit.single)
+
+edges.med.prosocial <- regmed.edges(fit.single, type = "mediators")
+plot.regmed.edges(edges.med.prosocial)
+
+edges.med <- regmed.edges(fit.best.prosocial, type = "mediators")
+class(edges.med)
 plot.regmed.edges(edges.med)
+class(fit.best.prosocial)
 #no mediators with individual behaviours 
 
-edges.any <- regmed.edges(fit.best, type = "any")
+edges.any <- regmed.edges(fit.best.prosocial, type = "any")
 plot.regmed.edges(edges.any)
-#socioeconomic position is related to recreational screen time and structured moderate-vigorous physical activity 
+
+edges.any <- regmed.edges(fit.single, type = "any")
+plot.regmed.edges(edges.any)
+
+
+
+#Repeat above for other outcome variables 
+male_lasso_hyperactivity <- list(exposure_variable, mediators_interactions, hahypr)
+names(male_lasso_hyperactivity) <- c("exposure", "mediator", "outcome")
+x <- male_lasso_hyperactivity$exposure
+y <- male_lasso_hyperactivity$outcome
+med <- male_lasso_hyperactivity$mediator
+
+data(male_lasso_prosocial)
+dat.filter.prosocial <- regmed.prefilter(x, med, y)
+
+lambda.grid <- seq(from = 0.4, to = 0.01, by = -0.01)
+
+x1 <- dat.filter.prosocial$x
+y1 <- dat.filter.prosocial$y
+med <- dat.filter.prosocial$mediator
+
+fit.male.prosocial <- regmed.grid(x1, med, y1, lambda.grid, frac.lasso = 0.8)
