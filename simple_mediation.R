17bb5ec7f39b7709db0a4326a920d04756c39f3e
katrina-wilhite <62529848+katrina-wilhite@users.noreply.github.com>
2022-09-02 13:35:35 +1000

    Convert variables to factors

--- simple_mediation.R
+++ simple_mediation.R
@@ -8,10 +8,7 @@ library(dplyr)
 library(haven)
 
 #Load male dataset from Study 2, LSAC wave 6 data, and LSAC wave 8 data 
-<<<<<<< HEAD
-#Update file path 
-=======
->>>>>>> 05627d727055ab59f9ab00110e25336b72666e6d
+
 load(file = "Z:/LSAC dataset/Study_2/Study_2/df_males_domsp_weekday.RData") 
 
 lsac_wave6 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb10.sas7bdat")
@@ -26,7 +23,7 @@ df_male_domsp_weekday %>%
 lsac_wave6 %>% 
   select(hicid, fcnfsad2d, fcnfsda2d, fcnfser2d, fcnfseo2d, fmoth, ffath, fsc13a1i, zf09fm, zf09ff) -> wave_6_relevant_data
 #Update column names to make sense 
-colnames(wave_6_relevant_data)[2:10] <- c("advantage_disadvantage", "disadvantage", "resources", "education_occupation", "mother_home", "father_home", "mental_health", "mother_race", "father_race")
+colnames(wave_6_relevant_data)[2:10] <- c("adv", "dis", "resources", "education_occupation", "mother_home", "father_home", "mental_health", "mother_race", "father_race")
 
 #Select relevant columns from LSAC Wave 8 (SDQ results) 
 lsac_wave8 %>% 
@@ -38,37 +35,40 @@ colnames(wave_8_relevant_data)[2:7] <- c("prosocial", "hyperactivity", "emotiona
 inner_join(relevant_male_data, wave_6_relevant_data, by = "hicid") -> domsp_wave6
 
 inner_join(domsp_wave6, wave_8_relevant_data, by = "hicid") -> df_males
+
+sapply(df_males, class)
+class(df_males$adv)
+
+for (x in c(2,4:5,7:15(df_males))) {
+  
+}
+
+df_males$adv <- as.factor(df_males$adv)
 #Check dataframe for missing data 
 colSums(is.na(df_males))
 #11 participants are missing SDQ data - this accounts for <1% of data and this information is essential for data anlaysis - therefore these participants will be removed from the dataset 
 df_males_na_removed <- na.omit(df_males)
 
+as.factor(df_males_na_removed$ad)
 
 df_males_na_removed$model3_trajectory_assignments <- as.factor(df_males_na_removed$model3_trajectory_assignments) 
 #Run analysis 
 df_males_na_removed[,"prosocial"] %>% 
   mutate_at(("prosocial"), ~(scale(.) %>% as.vector)) -> df_males_scaled
 
-<<<<<<< HEAD
-#I tried to "fit" a model but this did not solve the error "Error in varterms[sapply(cov, grep, varterms)] : invalid subscript type 'list'"
-=======
-#Try to fit model to population to see if this will fix error in "neEffdecomp" line
->>>>>>> 05627d727055ab59f9ab00110e25336b72666e6d
-#expFit <- glm(model3_trajectory_assignments*SEP ~ Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
-expData <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
-neProsocial <- neModel(prosocial ~ SEP0 + SEP1 + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, expData = expData, se = "robust")
+#expFit <- glm(model3_trajectory_assignments*SEP ~ Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
+expData <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
+neProsocial <- neModel(prosocial ~ SEP0 + SEP1 + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, expData = expData, se = "robust")
 summary(neProsocial)
 str(neProsocial)
-<<<<<<< HEAD
+
 #I trired to "unlist" the results but this did not solve the error
 #unlist(neProsocial) -> neProsocial2
 head(neProsocial)
-=======
 
->>>>>>> 05627d727055ab59f9ab00110e25336b72666e6d
 effdecomp <- neEffdecomp(neProsocial)
 summary(effdecomp)
 
 summary(expData)
 head(expData)
-expData2 <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + advantage_disadvantage + disadvantage + resources + education_occupation + mother_home + father_home + mental_health + mother_ethnicity + father_ethnicity, family = poisson(link = "log"), data = df_males_na_removed)
+expData2 <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_ethnicity + father_ethnicity, family = poisson(link = "log"), data = df_males_na_removed)
