c963340a9273ddab3009aeeb47d95cdd178e49cb
Katrina Wilhite <katrina.wilhite@gmail.com>
2022-09-03 15:59:50 +1000

    Ran full simple mediation analysis for males

--- simple_mediation.R
+++ simple_mediation.R
@@ -1,90 +1,93 @@
-#Simple Mediation 
+# Simple Mediation
 
-#Install appropriate libraries
-#install.packages("medflex")
-#Open libraries 
+# Install appropriate libraries
+# install.packages("medflex")
+# Open libraries
 library(medflex)
 library(dplyr)
 library(haven)
+# install.packages("remotes")
+#remotes::install_github("r-lib/styler")
+#install.packages("styler")
 
-#Load male dataset from Study 2, LSAC wave 6 data, and LSAC wave 8 data 
-
-load(file = "Z:/LSAC dataset/Study_2/Study_2/df_males_domsp_weekday.RData") 
+# Load male dataset from Study 2, LSAC wave 6 data, and LSAC wave 8 data
+load(file = "Z:/LSAC dataset/Study_2/Study_2/df_males_domsp_weekday.RData")
 
 lsac_wave6 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb10.sas7bdat")
 
 lsac_wave8 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb14.sas7bdat")
 
-#Select relevant columns from male dataset (hicid, profile membership, age, remoteness, Indigenous status, SEP)
-df_male_domsp_weekday %>% 
+# Select relevant columns from male dataset (hicid, profile membership, age, remoteness, Indigenous status, SEP)
+df_male_domsp_weekday %>%
   select(hicid, model3_trajectory_assignments, Age, Remoteness, Indigenous, SEP) -> relevant_male_data
 
-#Select relevant columns from LSAC wave 6 (SEIFA varialbes), parents living at home, parent mental health, parent ethnicity)  
-lsac_wave6 %>% 
+# Select relevant columns from LSAC wave 6 (SEIFA varialbes), parents living at home, parent mental health, parent ethnicity)
+lsac_wave6 %>%
   select(hicid, fcnfsad2d, fcnfsda2d, fcnfser2d, fcnfseo2d, fmoth, ffath, fsc13a1i, zf09fm, zf09ff) -> wave_6_relevant_data
-#Update column names to make sense 
+
+# Update column names to make sense
 colnames(wave_6_relevant_data)[2:10] <- c("adv", "dis", "resources", "education_occupation", "mother_home", "father_home", "mental_health", "mother_race", "father_race")
 
-#Select relevant columns from LSAC Wave 8 (SDQ results) 
-lsac_wave8 %>% 
+# Select relevant columns from LSAC Wave 8 (SDQ results)
+lsac_wave8 %>%
   select(hicid, hapsoc:hasdqtb) -> wave_8_relevant_data
-#Update column names to make sense
+
+# Update column names to make sense
 colnames(wave_8_relevant_data)[2:7] <- c("prosocial", "hyperactivity", "emotional", "peer", "conduct", "total")
 
-#Combine columns to make new dataset 
+# Combine columns to make new dataset
 inner_join(relevant_male_data, wave_6_relevant_data, by = "hicid") -> domsp_wave6
-
 inner_join(domsp_wave6, wave_8_relevant_data, by = "hicid") -> df_males
 
+#Take out participants with missing covariate data 
 colSums(df_males < -8)
-#2 mental health, 9 mother race, 125 father race
-df_males <- df_males[!(df_males$mental_health == -9),]
-df_males <- df_males[!(df_males$mother_race == -9),]
-df_males <- df_males[!(df_males$father_race == -9),]
+# 2 mental health, 9 mother race, 125 father race
+df_males <- df_males[!(df_males$mental_health == -9), ]
+df_males <- df_males[!(df_males$mother_race == -9), ]
+df_males <- df_males[!(df_males$father_race == -9), ]
 
-#Check dataframe for missing data 
+# Check dataframe for missing data reported as N/A
 colSums(is.na(df_males))
-#11 participants are missing SDQ data - this accounts for <1% of data and this information is essential for data anlaysis - therefore these participants will be removed from the dataset 
+# 11 participants are missing SDQ data - this accounts for <1% of data and this information is essential for data anlaysis - therefore these participants will be removed from the dataset
 df_males_na_removed <- na.omit(df_males)
 
+#Reduce mother and father race to 1 digit and convert to factor 
+df_males_na_removed$mother_race <- substr(df_males_na_removed$mother_race, 1, 1) %>% 
+  as.factor()
+df_males_na_removed$father_race <- substr(df_males_na_removed$father_race, 1, 1) %>% 
+  as.factor()
 
-#Covert factor variables to factor 
-df_males_na_removed$adv <- as.factor(df_males_na_removed$adv)
-df_males_na_removed$dis <- as.factor(df_males_na_removed$dis)
-df_males_na_removed$model3_trajectory_assignments <- as.factor(df_males_na_removed$model3_trajectory_assignments)
-df_males_na_removed$Remoteness <- as.factor(df_males_na_removed$Remoteness)
-df_males_na_removed$Indigenous <- as.factor(df_males_na_removed$Indigenous)
-df_males_na_removed$resources <- as.factor(df_males_na_removed$resources)
-df_males_na_removed$education_occupation <- as.factor(df_males_na_removed$education_occupation)
-df_males_na_removed$mother_home <- as.factor(df_males_na_removed$mother_home)
-df_males_na_removed$father_home <- as.factor(df_males_na_removed$father_home)
-df_males_na_removed$mother_race <- as.factor(df_males_na_removed$mother_race)
-df_males_na_removed$father_race <- as.factor(df_males_na_removed$father_race)
-df_males_na_removed$mental_health <- as.factor(df_males_na_removed$mental_health)
+# Covert relevant columns to factors 
+factor_columns <- c(2,4:5,7:13)
+for (x in factor_columns) {
+  name <- colnames(df_males_na_removed[,x])
+ df_males_na_removed[[name]] <- as.factor(df_males_na_removed[[name]])
+}
 
-#Run analysis 
-df_males_na_removed$prosocial <- scale(df_males_na_removed$prosocial)
+#Scale all socio-emotional outcome variables 
+df_males_na_removed[,16:21] <- scale(df_males_na_removed[,16:21])
 
-df_males_na_removed$mother_race <- df_males_na_removed$mother_race
-df_males_na_removed$mother_race <- substr(df_males_na_removed$mother_race, 1, 1)
-df_males_na_removed$father_race <- substr(df_males_na_removed$father_race, 1,1)
-df_males_na_removed$mother_race <- as.factor(df_males_na_removed$mother_race)
-df_males_na_removed$father_race <- as.factor(df_males_na_removed$father_race)
-sapply(df_males_na_removed, class)
+#Run loop for all socio-emotional variables 
+for (x in 16:ncol(df_males_na_removed)) {
+  name <- colnames(df_males_na_removed[,x])
+  assign("objectname", (paste(name,"~",("SEP + model3_trajectory_assignments + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race"))))
+  expData <- assign(paste0("expData_", name), neImpute(objectname, data = df_males_na_removed))
+  assign("model", (paste(name,"~",("SEP0 + SEP1 + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race"))))
+  outcome_results <- assign(paste0("ne_", name), neModel(model, expData = expData, se = "robust"))
+  assign(paste0("effdecomp_", name), neEffdecomp(outcome_results))
+  }
 
-#expFit <- glm(model3_trajectory_assignments*SEP ~ Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, data = df_males_na_removed)
-expData <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race,  data = df_males_na_removed)
-neProsocial <- neModel(prosocial ~ SEP0 + SEP1 + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_race + father_race, expData = expData, se = "robust")
-summary(neProsocial)
-str(neProsocial)
+#Check results for mediation 
+summary(effdecomp_prosocial)
+#No mediation with prosocial behaviour 
+summary(effdecomp_hyperactivity)
+#No mediation with hyperactivity 
+summary(effdecomp_emotional)
+#No mediation with emotional problems 
+summary(effdecomp_peer)
+#No mediation with peer problems 
+summary(effdecomp_conduct)
+#No mediation with conduct problems 
+summary(effdecomp_total)
+#No mediation with total strengths and difficulties 
 
-#I trired to "unlist" the results but this did not solve the error
-#unlist(neProsocial) -> neProsocial2
-head(neProsocial)
-
-effdecomp <- neEffdecomp(neProsocial)
-summary(effdecomp)
-
-summary(expData)
-head(expData)
-expData2 <- neImpute(prosocial ~ SEP + model3_trajectory_assignments + Remoteness + Indigenous + adv + dis + resources + education_occupation + mother_home + father_home + mental_health + mother_ethnicity + father_ethnicity, family = poisson(link = "log"), data = df_males_na_removed)
