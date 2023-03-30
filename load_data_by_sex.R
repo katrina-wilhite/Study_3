ecf736c2ad71e2fafc1f9ff8723204057159bafc
Katrina Wilhite <katrina.wilhite@gmail.com>
2023-03-30 13:27:16 -0400

    Update cloud_auth and retrieve datasets

--- load_data_by_sex.R
+++ load_data_by_sex.R
@@ -1,8 +1,10 @@
 library(haven)
 install.packages("cloudstoR")
 library("cloudstoR")
-cloud_auth()
-cloud_browse(path = "")
+cloud_auth(reset_keys = TRUE)
+
+cloud_get(path = "Shared/LSAC - Katrina/Study_2/Study_2/df_males_domsp_weekday.RData")
+cloud_list(path = 'cloudstoR')
 
 male_data <- function(){
   load(file = "Z:/LSAC dataset/Study_2/Study_2/df_males_domsp_weekday.RData", envir = globalenv())
@@ -17,4 +19,17 @@ female_data <- function(){
   .GlobalEnv$lsac_wave8 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb14.sas7bdat")
 }
 
+cloud_get(path = "Shared/LSAC - Katrina/Study_2/Study_2/df_females_domsp_weekday.RData")
+#Updated code retrieved from cloudstoR
+male_data <- function(){
+  cloud_get(path = "Shared/LSAC - Katrina/Study_2/Study_2/df_males_domsp_weekday.RData")
+  .GlobalEnv$lsac_wave6 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb10.sas7bdat")
+  .GlobalEnv$lsac_wave8 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb14.sas7bdat")
+}
 
+
+female_data <- function(){
+  cloud_get(path = "Shared/LSAC - Katrina/Study_2/Study_2/df_females_domsp_weekday.RData")
+  .GlobalEnv$lsac_wave6 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb10.sas7bdat")
+  .GlobalEnv$lsac_wave8 <- read_sas("Z:/LSAC dataset/General Release/Survey data/SAS/lsacgrb14.sas7bdat")
+}
